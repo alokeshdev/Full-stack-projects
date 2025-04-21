45fc1a37af46d57fdc518c6d61c0dd4ccd0da3f6
alokeshdev <alokeshbiswas4444@gmail.com>
2025-04-21 12:06:06 +0530

    Add files via upload

---
+++ signifiya/src/models/register.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const {model} = require("mongoose");
+
+const registerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    roll_no: {
+        type: String,
+        required: true
+    },
+    reg_no: {
+        type: String,
+        required: true
+    },
+    event_info: {
+        type: String,
+        required: true
+    }
+});
+
+const Register = mongoose.model("Register", registerSchema);
+
+module.exports=Register;
